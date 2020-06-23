ac5ceefe319839364922e83839a4484d67c6703d
JackXu0 <627054436@qq.com>
2020-06-23 16:51:54 -0400

    aa

--- contact_shield_demo/app/src/main/java/com/futurewei/contact_shield_demo/network/upload_periodic_key.java
+++ contact_shield_demo/app/src/main/java/com/futurewei/contact_shield_demo/network/upload_periodic_key.java
@@ -7,6 +7,9 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,6 +20,7 @@ import java.util.List;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -28,10 +32,13 @@ public class upload_periodic_key extends  Thread{
 
     public Context context;
     public Handler handler;
+    public JsonArray jsonArray;
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-    public upload_periodic_key(Context context, Handler handler){
+    public upload_periodic_key(Context context, Handler handler, JsonArray jsonArray){
         this.context = context;
         this.handler = handler;
+        this.jsonArray = jsonArray;
     }
 
     @Override
@@ -41,17 +48,12 @@ public class upload_periodic_key extends  Thread{
         final Message msg=new Message();
         msg.what=1;
 
-        final SharedPreferences sharedPreferences = context.getSharedPreferences("last_download_timeStamp",MODE_PRIVATE);
-        int last_download_timeStamp=sharedPreferences.getInt("last_download_timeStamp",0);
-
-        RequestBody formBody = new FormBody.Builder()
-//                .add("timeStamp", ""+last_download_timeStamp)
-                .build();
+        RequestBody body = RequestBody.create(jsonArray.toString(), JSON);
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("http://10.0.2.2:8000/downloadNew")
-                .post(formBody)
+                .post(body)
                 .build();
 
         Call call=client.newCall(request);
@@ -60,7 +62,7 @@ public class upload_periodic_key extends  Thread{
         call.enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.e("dcddcdcdc","3");
+                Log.e("upload periodic key","on Failure");
                 Bundle b =new Bundle();
                 b.putInt("response_code",0);
                 msg.setData(b);
@@ -70,13 +72,13 @@ public class upload_periodic_key extends  Thread{
             @Override
             public void onResponse(Call call, final Response response) throws IOException {
                 if(response.isSuccessful()){
-                    Log.e("dcddcdcdc","4");
+                    Log.e("upload periodic key","response and success");
                     Bundle b =new Bundle();
                     b.putInt("response_code",1);
                     msg.setData(b);
                     handler.sendMessage(msg);
                 }else{
-                    Log.e("dcddcdcdc","3");
+                    Log.e("upload periodic key","response but failed");
                     Bundle b =new Bundle();
                     b.putInt("response_code",2);
                     msg.setData(b);
